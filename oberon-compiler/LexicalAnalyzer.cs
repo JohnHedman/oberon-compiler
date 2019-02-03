166943fe074d7922d4743f28a754922a634e9646
JohnHedman <Jonathan.Hedman@sdstate.edu>
2019-02-02 20:41:41 -0600

    Created Lexical Analyzer class, worked on getting Oberon file from user.

---
+++ oberon-compiler/LexicalAnalyzer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oberon_compiler
+{
+    class LexicalAnalyzer
+    {
+
+    }
+}
--- oberon-compiler/Program.cs
+++ oberon-compiler/Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace oberon_compiler
 {
     class Program
     {
+        const string debug_directory = "../";
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string oberonFile = debug_directory + DecodeArgs(args);
+
+            string[] oberon_lines = System.IO.File.ReadAllLines(oberonFile);
+
+            for(int x = 0; x < oberon_lines.Length; x++)
+            {
+                Console.WriteLine(oberon_lines[x]);
+            }
+
+
+            Console.Write("\nPress a key to end... ");
+            Console.ReadKey();
         }
+
+
+        // Read command line arguments and find the oberon file to compile
+        static string DecodeArgs(string[] arguments)
+        {
+            string oberonFile;
+
+            if(arguments.Length == 1)
+            {
+                oberonFile = arguments[0];
+            }
+            else
+            {
+                Console.Write("What file should I read from: ");
+                oberonFile = Console.ReadLine();
+            }
+
+            Console.WriteLine("Gotcha, reading from: " + oberonFile + "\n");
+
+            return oberonFile;
+        }
+
+
+
+
     }
 }
+
+
+
